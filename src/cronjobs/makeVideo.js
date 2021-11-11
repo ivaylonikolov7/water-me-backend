c3bdb5d9301a79085f7e48945a08fc2c1b7608d1
Ivaylo Nikolov <ivaylonikolov@Ivaylos-MacBook-Air.local>
2021-11-11 02:16:29 +0200

    use fluent-ffmpeg

--- src/cronjobs/makeVideo.js
+++ src/cronjobs/makeVideo.js
@@ -1,10 +1,12 @@
-const videoshow = require('videoshow');
+var ffmpeg = require('fluent-ffmpeg');
+var command = ffmpeg();
+
 const fsPromises = require('fs').promises;
 const cron = require('node-cron');
 
 async function makeVideo(){
 	let images = await getImagesFolder();
-	const videoOptions = {
+	/* const videoOptions = {
 		fps: 1,
 		transition: false,
 		videoBitrate: 1024,
@@ -14,21 +16,30 @@ async function makeVideo(){
 		audioChannels: 2,
 		format: 'mp4',
 		pixelFormat: 'yuv420p'
-	}
-	console.log('images');
-    let result = await getImagesFolder();
-	console.log(result)
-
-	videoshow(images, videoOptions)
-	.save('video.mp4').on('start', ()=>{
-		console.log('start');
-	}).on('error', (err, stdout, stderr)=>{
-		console.error('Error:', err)
-		console.error('ffmpeg stderr:', stderr)
-		console.error('ffmpeg stderr:', stdout)
-	}).on('end', ()=>{
-		console.log('end');
+	} */
+	
+	command.input('./src/images/%*.png')
+	.videoCodec('libx264')
+	.audioCodec('libmp3lame')
+	.size('683x384')
+	.on('error', (err)=>{
+		console.log(err)
 	})
+	.on('end', ()=>{
+		console.log('done');
+	})
+	.save('output.mp4')
+
+	// videoshow(images, videoOptions)
+	// .save('video.mp4').on('start', ()=>{
+	// 	console.log('start');
+	// }).on('error', (err, stdout, stderr)=>{
+	// 	console.error('Error:', err)
+	// 	console.error('ffmpeg stderr:', stderr)
+	// 	console.error('ffmpeg stderr:', stdout)
+	// }).on('end', ()=>{
+	// 	console.log('end');
+	// })
 }
 
 
